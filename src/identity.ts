c8bc452d69d76034acb19b8dd752b9e8a8d8ce42
ElRaptorus <christian.werner@5minds.de>
2018-06-27 09:56:16 +0200

    Remove identity implementation

--- src/identity.ts
+++
@@ -1,9 +0,0 @@
-import {IIdentity} from './iidentity';
-
-export class Identity implements IIdentity {
-  public token: string;
-
-  constructor(token: string) {
-    this.token = token;
-  }
-}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-export * from './identity';
 export * from './iiam_configuration';
 export * from './iiam_service';
 export * from './iidentity_service';
-export * from './identity';
+export * from './iidentity';
 export * from './iintrospect_response';
