41f0e62c6b8bbe549143a68cd2f3760f20f36149
ElRaptorus <christian.werner@5minds.de>
2019-01-16 10:56:31 +0100

    :sparkles: Add the userId property to the identity.

--- src/iidentity.ts
+++ src/iidentity.ts
@@ -1,3 +1,13 @@
+/**
+ * Contains information about an identity.
+ */
 export interface IIdentity {
+  /**
+   * The ID of the user to which this identiy belongs.
+   */
+  userId: string;
+  /**
+   * The AuthToken connected to this identity.
+   */
   token: string;
 }
